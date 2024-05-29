a4ea716d5b2bd7a96f01ddb0fcf449dddff9fcb7
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-05-29 19:33:31 +0300

    ср "Шаблонизатор"

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -1,18 +1,29 @@
 package org.example.hexlet;
 
 import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+import static io.javalin.rendering.template.TemplateUtil.model;
+import org.example.hexlet.model.Course;
+import org.example.hexlet.dto.courses.CoursesPage;
+
+import java.util.List;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        // Создаем приложение
         var app = Javalin.create(config -> {
             config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
         });
-        // Описываем, что загрузится по адресу /
-        app.get("/hello", ctx -> {
-            var name = ctx.queryParam("name");
-            ctx.result(name != null ? "Hello, " + name + "!" : "Hello, World!");
+
+        app.get("/courses", ctx -> {
+            var courses = List.of(new Course("java","Курсы по программированию на java"),
+                    new Course("php", "На этом курсе вы изучите основы языка PHP"),
+                    new Course("HTML", "Язык разметки веб-страниц"));
+            var header = "Курсы по программированию";
+            var page = new CoursesPage(courses, header);
+            ctx.render("courses/index.jte", model("page", page));
         });
-        app.start(7070); // Стартуем веб-сервер
+
+        app.start(7070);
     }
 }
